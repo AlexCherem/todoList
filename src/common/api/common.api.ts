457fddafa7f579887e633f00c4a77c398909f07a
AlexCherem <mojasemja678@gmail.com>
2024-07-09 14:33:07 +0300

    api-key

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -4,6 +4,6 @@ export const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.1/",
   withCredentials: true,
   headers: {
-    "API-KEY": "360b05d5-5c97-4915-ad7f-12a66e5d8c68",
+    "API-KEY": "'d0ce4cd8-d0d2-4152-99ad-f6e1407cb23f",
   },
 });
